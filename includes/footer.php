ee06b737f71db92def699d4a1dddda1a98ea5eea
bram <35409298+Bram0202@users.noreply.github.com>
2019-02-28 21:08:29 +0100

    v2.1
    Goede Bitcoin logo.
    's achter satoshi bij hoeveelheden.

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+<head>
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/footer.css">
+</head>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-3 offset-md-1 my-bg">
+      <!-- Twitter link. -->
+      <p>Created by: <a href="https://twitter.com/brambakx" target="_blank">Bram Bakx</a></p>
+    </div>
+
+    <div class="col-md-3 my-bg">
+      <!-- Coindesk link. -->
+      <p>Powered by: <a href="https://www.coindesk.com/price/bitcoin" target="_blank">CoinDesk</a></p>
+    </div>
+
+    <div class="col-md-3 my-bg">
+      <a href="https://tippin.me/@brambakx" target="_blank">Want to support me? ➡️</a>
+
+    </div>
+    <div class="col-md-2 my-bg">
+
+      <!-- Beginning of tippin.me Button -->
+    <div id="tippin-button" data-dest="brambakx"></div>
+    <script src="https://tippin.me/buttons/tip.js" type="text/javascript"></script>
+    <!-- End of tippin.me Button -->
+  </div>
+
+  </div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,40 @@
+
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/header.css">
+
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i,600|Montserrat:400,700" rel="stylesheet">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  </head>
+
+     <nav class="navbar navbar-expand-md navbar-light">
+
+      <!-- Logo. -->
+      <a class="my-logo" href="index" class="navbar-brand">The <span class="orange">Bitcoin</span> price in satoshi's</a>
+
+      <!-- Hamburger-menu. -->
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Main menu. -->
+      <div class="collapse navbar-collapse" id="navbarMenu">
+        <ul class="navbar-nav ml-auto">
+
+          <li class="nav-item">
+            <a class="nav-link" id="<?php if($page == 'about'){echo 'active';}?>" href="about">About</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link my-link" href="https://twitter.com/intent/tweet?text=Did%20you%20know%20you%20can%20just%20buy%20a%20part%20of%20a%20bitcoin?%20This%20website%20shows%20fiat%20vs.%20Bitcoin%20in%20satoshi!%20https%3A%2F%2Fsatoshisprice.com" target="_blank">Share on Twitter</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
