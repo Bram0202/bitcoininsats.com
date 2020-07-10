21a50ecd6dd0d0b78899d92c7f5dd42203ed588a
Bram <brambakx@pm.me>
2020-07-10 20:11:48 +0200

    Added readme link

--- includes/footer.php
+++ includes/footer.php
@@ -5,17 +5,18 @@
 
 <div class="container-fluid">
   <div class="row">
-    <div class="col-md-3 offset-md-1 my-bg">
-      <!-- Twitter link. -->
+
+    <div class="col-md-3 offset-md-2 my-bg">
       <p>Created by: <a href="https://twitter.com/bram21_" target="_blank">Bram</a></p>
     </div>
 
     <div class="col-md-3 my-bg">
-      <!-- Coindesk link. -->
       <p>Data by: <a href="https://www.coindesk.com/price/bitcoin" target="_blank">CoinDesk</a></p>
     </div>
 
-
+    <div class="col-md-3 my-bg">
+      <p><a href="https://github.com/Bram0202/satoshi-s-price/blob/master/README.md" target="_blank">README</a></p>
+    </div>
 
   </div>
 </div>
