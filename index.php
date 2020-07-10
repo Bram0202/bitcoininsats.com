1af9de6cc152ea1aa9a4745f9e7f7d4d65cf688b
Bram <brambakx@pm.me>
2020-07-10 19:41:19 +0200

    Added favicon

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="apple-touch-icon" type="image/png" sizes="180x190" href="images/apple-touch-icon.png">
+
     <link rel="stylesheet" href="css/main.css">
 
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i,600|Montserrat:400,700" rel="stylesheet">
